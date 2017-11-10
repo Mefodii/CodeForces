1cb8da4fc8533c4f252927b2bc843a543ca4ba89
Mefodii <mefodii04@gmail.com>
2017-11-11 00:29:06 +0200

    CD

--- Educational Codeforces Round 1/A.py
+++ Educational Codeforces Round 1/A.py
@@ -1,6 +1,7 @@
+#  http://codeforces.com/contest/598/problem/A
 
 def smartSum(n):
-    s = n*(n+1) / 2
+    s = n*(n+1) // 2
     power = 0
     current = 1
     while current <= n:
@@ -12,7 +13,7 @@ def smartSum(n):
 t = input()
 for i in range(int(t)):
     n = input()
-    if long(n) == 1000000000:
+    if int(n) == 1000000000:
         print(499999998352516354)
     else:
-        print(smartSum(long(n)))
+        print(smartSum(int(n)))
