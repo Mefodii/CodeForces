3dd86f6014da89911669e38830bb458f56aff461
Mefodii <mefodii04@gmail.com>
2017-11-14 17:09:17 +0200

    Try

---
+++ Educational Codeforces Round 1/B.py
@@ -0,0 +1,9 @@
+#  http://codeforces.com/contest/598/problem/B
+
+
+string = input()
+print(string[1::2])
+# operations = int(input())
+# for i in range(operations):
+#     startChar, endChar, shift = map(int, input().split())
+
